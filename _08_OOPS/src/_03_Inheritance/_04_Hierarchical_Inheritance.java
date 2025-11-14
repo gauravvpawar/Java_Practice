078fc07984097ad4d26ba8fa0c6e680bd215a747
Gaurav <gauravkailaspawar101@gmail.com>
2025-11-14 16:52:49 +0530

    Understanding about the hierarichical inheritance and hybrid inheritance

--- _08_OOPS/src/_03_Inheritance/_04_Hierarchical_Inheritance.java
+++ _08_OOPS/src/_03_Inheritance/_04_Hierarchical_Inheritance.java
@@ -1,6 +1,24 @@
 package _03_Inheritance;
 
+class Dmart
+{
+    int pen = 10 , Biscuit = 50 , clothes = 500;
+}
 
+class  Customer1 extends Dmart{
+    void fun()
+    {
+        System.out.println("total :  " + (pen + Biscuit));
+    }
+}
+
+class Customer2 extends Dmart
+{
+    void total()
+    {
+        System.out.println("Total : "  + (pen + Biscuit +clothes));
+    }
+}
 
 public class _04_Hierarchical_Inheritance
 {
@@ -8,5 +26,11 @@ public class _04_Hierarchical_Inheritance
         /*
         Hierarchical inheritance have one parent class and multiple child class
          */
+
+        Customer1 c1 = new Customer1();
+        c1.fun();
+
+        Customer2 c2 = new Customer2();
+        c2.total();
     }
 }
---
+++ _08_OOPS/src/_03_Inheritance/_05_Hybrid_Inheritance.java
@@ -0,0 +1,11 @@
+package _03_Inheritance;
+
+public class _05_Hybrid_Inheritance
+{
+    public static void main(String[] args) {
+        /*
+           - Java does not support Hybrid inheritance
+           - java do not allow one class have multiple parent due to ambiguity problem
+         */
+    }
+}
